50ec5c2e218691cd100900d83c2c3e116398bde6
Logic jedi <waweruisaac25@gmail.com>
2024-05-02 19:07:03 +0300

    type_system fixed

--- dart_type_system/type.dart
+++ dart_type_system/type.dart
@@ -1,3 +1,22 @@
 void main() {
   // TODO: Implement dart type system
+  print('say something!');
+  var name = 'hi';
+  print(name);
+  const number = 80;
+  print(number);
+
+  const text = 'I like pizza';
+  const topping = 'with tomatoes';
+  const favourite = '$text $topping';
+  final newText = favourite.replaceAll('pizza', 'pasta');
+  const newfavourite = 'Now I like curry';
+  print(newText);
+  print(newfavourite);
+
+  // var x = 10;
+  // x = true;
+  dynamic n = 10;
+  n = true;
+  print(n);
 }
