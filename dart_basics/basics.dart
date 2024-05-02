09b5b66dc838197ad8d6fe27aaa2eced654e7fa1
Logic jedi <waweruisaac25@gmail.com>
2024-05-02 18:15:24 +0300

    always commit greatness

--- dart_basics/basics.dart
+++ dart_basics/basics.dart
@@ -1,12 +1,51 @@
 void main() {
-  double temperature = 20;
-  int value = 8;
-  String pizza = 'pizza';
-  String pasta = 'pasta';
+/* 
+ * Version 1.5 will include:
+ * - User authentication with JWT
+ * - sorting results by date
+ * - generate reports
+ * - build the best software in the world
+ * - deploy on the internet
+*/
 
-  print('The temperature is ${temperature}C');
-  print('${value} pluus ${value} makes ${value + value}');
-  print('I like ${pizza} and ${pasta}');
+  // TODO: Implement me
+  // TODO: Write cool documentation for this app
+
+  ///Ternary access operator
+  // int age = 12;
+  // String type = age > 16 ? 'adult' : 'child';
+  // print(type);
+
+  ///Arithmetic Operators
+  // double tempFarenheit = 90;
+  // double tempCelsius = (tempFarenheit - 32) / 1.8;
+  // print('${tempFarenheit}F = ${tempCelsius.toStringAsFixed(1)}C');
+
+  ///Contains & replaceAll
+  // String foodPreference = 'I like pizza';
+  // print(foodPreference.contains('pizza'));
+  // print(foodPreference.replaceAll('pizza', 'pasta'));
+
+  ///Methods & Assignment
+  // String title = 'Dart course'.toLowerCase();
+  // print(title);
+  // title = title.toUpperCase();
+  // print(title);
+
+//   print("""
+//   Use in Adults For vaginal candidiasis, fluconazole 150 mg (DIFLUCAN® ONE) should be administered as a single oral dose.
+//   The median time to onset of symptom relief following a 150 mg single oral dose for the treatment of vaginal candidiasis is one day.
+// """);
+
+  ///TOPIC 2
+  // double temperature = 20;
+  // int value = 8;
+  // String pizza = 'pizza';
+  // String pasta = 'pasta';
+
+  // print('The temperature is ${temperature}C');
+  // print('$value pluus $value makes ${value + value}');
+  // print('I like $pizza and $pasta');
 
   ///TOPIC 1
   // String firstName = 'Andrea'; //Variable declaration and assignment
