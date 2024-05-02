f2ad5d6bab1bcb9622edc75e1f6b7d2deff29ebb
Logic jedi <waweruisaac25@gmail.com>
2024-05-02 16:02:07 +0300

    initial commit

---
+++ dart_basics/basics.dart
@@ -0,0 +1,24 @@
+void main() {
+  double temperature = 20;
+  int value = 8;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  print('The temperature is ${temperature}C');
+  print('${value} pluus ${value} makes ${value + value}');
+  print('I like ${pizza} and ${pasta}');
+
+  ///TOPIC 1
+  // String firstName = 'Andrea'; //Variable declaration and assignment
+  // String lastName = 'Bizzotto';
+  // int age = 36;
+  // double height = 1.84;
+  // bool isMarried = true;
+
+  // print(firstName);
+  // print(lastName);
+  // print(age);
+  // print(height);
+  // print(
+  //     'isMarried: $isMarried and is ${age + 1} years old next year.'); // String interpolation
+}
