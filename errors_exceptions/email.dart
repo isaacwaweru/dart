19381acb92b383f6f0aa7e64f1692d2356665d80
Logic jedi <waweruisaac25@gmail.com>
2024-05-07 15:21:28 +0300

    exceptions

---
+++ errors_exceptions/email.dart
@@ -0,0 +1,28 @@
+class EmailAddress {
+  EmailAddress(this.email) {
+    if (email.isEmpty) {
+      throw FormatException('email can\'t be empty');
+    }
+
+    if (!email.contains('@')) {
+      throw FormatException('$email doesn\'t contains the @ symbol');
+    }
+  }
+  String email;
+
+  @override
+  String toString() => email;
+}
+
+void main() {
+  try {
+    print(EmailAddress('me@example.com'));
+    print(EmailAddress('example.com'));
+    print(EmailAddress(''));
+  } on FormatException catch (e) {
+    print(e);
+  }
+  // Desired output:
+  // me@example.com
+  // FormatException: example.com doesn't contain the @ symbol
+}
---
+++ errors_exceptions/exceptions.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Fraction {
+  Fraction(this.numerator, this.denominator) {
+    if (denominator == 0) {
+      throw IntegerDivisionByZeroException();
+    }
+  }
+  final int numerator;
+  final int denominator;
+
+  double get value => numerator / denominator;
+}
+
+void testFraction() {
+  try {
+    final f = Fraction(3, 0);
+    print(f.value);
+  } on IntegerDivisionByZeroException catch (e) {
+    print(e);
+    rethrow;
+  } on Exception catch (e) {
+    print(e);
+    // TODO: handle Exception
+  } finally {
+    print('testFraction done');
+  }
+}
+
+void main() {
+  testFraction();
+  print('done');
+}
